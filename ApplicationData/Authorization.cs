88de14eb027ebb73936445c044ef57e046701ceb
Толмачев.Николай <.@TERM1>
2023-11-21 10:23:17 +0300

    Добавьте файлы проекта.

---
+++ ApplicationData/Authorization.cs
@@ -0,0 +1,53 @@
+﻿using SportingGoods.Model;
+using SportingGoods.View.Windows;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SportingGoods.ApplicationData
+{
+    class Authorization
+    {
+        public static User currentUser = new User();
+
+        public static bool Check(string login, string password)
+        {
+            currentUser = App.context.User.FirstOrDefault(user => user.Login == login && user.Password == password);
+
+            if(currentUser != null)
+            {
+                CaptchaWindow captchaWindow = new CaptchaWindow();
+
+                if(captchaWindow.ShowDialog() == true)
+                {
+                    switch (currentUser.IdRole)
+                    {
+                        case 1:
+                            AdministratorWindow administrator = new AdministratorWindow();
+                            administrator.Show();
+                            break;
+
+                        case 2:
+                            ManagerWindow manager = new ManagerWindow();
+                            manager.Show();
+                            break;
+
+                        case 3:
+                            ClienWindow clien = new ClienWindow();
+                            clien.Show();
+                            break;
+                    }
+                    return true;
+                }
+                else
+                {
+                    MessageBox.Show("Данные не верны. Возможно неверный логин или паролью");
+                }
+                return false;
+            }
+        }
+    }
+}
---
+++ View/Windows/AuthorizationWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using SportingGoods.ApplicationData;
+using SportingGoods.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SportingGoods.View.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для AuthorizationWindow.xaml
+    /// </summary>
+    public partial class AuthorizationWindow : Window
+    {
+
+        public AuthorizationWindow() 
+        {
+            InitializeComponent();
+        }
+
+        private void AuthorizationBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if(Authorization.Check(Convert.ToString(login.Text), PasswordPb.Password) == true)
+            {
+                DialogResult = true;
+            }
+        }
+
+        private void CancelBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
